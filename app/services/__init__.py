135cdfb7539feec772ac4ce2f8df750aba8f5a53
akov40320 <klymenkoyakov@gmail.com>
2025-08-23 10:32:25 +0300

    Add module docstring to services package init

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,9 @@
+"""Initialization for the services package.
+
+This package exposes selected helper functions for use throughout the
+application, such as :func:`tg_send_with_retry`.
+"""
+
 from .telegram import tg_send_with_retry
 
 __all__ = ["tg_send_with_retry"]
