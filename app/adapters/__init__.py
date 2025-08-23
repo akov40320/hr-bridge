7d35367b5013b40a97beb220f9faa2078da0d423
akov40320 <klymenkoyakov@gmail.com>
2025-08-23 10:39:38 +0300

    Add module docstring to adapters package

--- app/adapters/__init__.py
+++ app/adapters/__init__.py
@@ -1,1 +1,7 @@
+"""Collection of available integration adapters.
+
+This module exposes the list of public adapter modules that can be imported
+when using ``from app.adapters import *``.
+"""
+
 __all__ = ["hh", "avito", "amochats", "amo_client"]
